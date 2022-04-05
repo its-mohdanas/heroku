6f94b2afdda14b8909924d5220f2745753de453c
MOHD ANAS <63398514+its-mohdanas@users.noreply.github.com>
2022-04-05 21:46:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,24 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, url_for
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 app = Flask(__name__)
-
 
 path = 'D:\\Code\\WebDev\\chromedriver_win32\\chromedriver.exe'
 driver = webdriver.Chrome(path)
 
 scraped = []
-
-
-
-@app.route('/scraped')
-def scraper():
-    return jsonify(scraped)
+# @app.route('/scraped1')
+# def scraper():
+#     # return jsonify(scraped)
+#     return render_template('index.html')
 
 list1 = []
 
 ##############################################
-@app.route('/scraped', methods=['POST'])
+@app.route('/', methods=['GET'])
 def add_scrap():
     website="https://www.papersdrop.com/"
-    scrap = request.get_json()
+
     driver.get(website)
     links = driver.find_elements_by_tag_name('a')
 
@@ -31,6 +28,9 @@ def add_scrap():
     driver.close()
 
     dic1 = {"output": list1}
-    return jsonify(dic1)
+    # return jsonify(dic1)
+    print(jsonify(dic1))
+    # print(list1)
+    return render_template('index.html', dic1 = dic1)
 
-app.run()
+app.run(debug=True)
