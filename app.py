b561af62c4debe0a80408f0d5e2c142de10af97e
MOHD ANAS <63398514+its-mohdanas@users.noreply.github.com>
2022-04-06 21:12:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,37 @@
-from flask import Flask, request, jsonify, render_template, url_for
+from selenium import webdriver
+import os
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+
+from flask import Flask, request, jsonify, render_template
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 app = Flask(__name__)
+
 
 path = 'D:\\Code\\WebDev\\chromedriver_win32\\chromedriver.exe'
 driver = webdriver.Chrome(path)
 
 scraped = []
-# @app.route('/scraped1')
-# def scraper():
-#     # return jsonify(scraped)
-#     return render_template('index.html')
+
+
+
+@app.route('/scraped')
+def scraper():
+    return jsonify(scraped)
 
 list1 = []
 
 ##############################################
-@app.route('/', methods=['GET'])
+@app.route('/scraped', methods=['POST'])
 def add_scrap():
     website="https://www.papersdrop.com/"
-
+    scrap = request.get_json()
     driver.get(website)
     links = driver.find_elements_by_tag_name('a')
 
@@ -28,9 +41,6 @@ def add_scrap():
     driver.close()
 
     dic1 = {"output": list1}
-    # return jsonify(dic1)
-    print(jsonify(dic1))
-    # print(list1)
-    return render_template('index.html', dic1 = dic1)
+    return jsonify(dic1)
 
-app.run(debug=True)
+app.run()
